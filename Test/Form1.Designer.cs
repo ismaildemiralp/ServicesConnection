5c7640b83f240565a645ec0736e324901b548d1b
idemiralp <ismail.demiralpp@gmail.com>
2024-01-02 23:51:47 +0300

    Proje Eklendi
    Servis baglanti projesi eklendi

---
+++ Test/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnrabbit = new Button();
+            btnredis = new Button();
+            btnsql = new Button();
+            txtrabbit = new TextBox();
+            txtredis = new TextBox();
+            txtsqlswname = new TextBox();
+            txtsqlswpass = new TextBox();
+            txtsqlswuname = new TextBox();
+            chksqlswpass = new CheckBox();
+            btnELK = new Button();
+            txtELK = new TextBox();
+            txtIndex = new TextBox();
+            txtUname = new TextBox();
+            txtPassword = new TextBox();
+            txtLevel = new TextBox();
+            txtMessage = new TextBox();
+            btnelk_con = new Button();
+            chkelk = new CheckBox();
+            SuspendLayout();
+            // 
+            // btnrabbit
+            // 
+            btnrabbit.Location = new Point(40, 90);
+            btnrabbit.Name = "btnrabbit";
+            btnrabbit.Size = new Size(206, 81);
+            btnrabbit.TabIndex = 0;
+            btnrabbit.Text = "RABBITMQ";
+            btnrabbit.UseVisualStyleBackColor = true;
+            btnrabbit.Click += btnrabbit_Click;
+            // 
+            // btnredis
+            // 
+            btnredis.Location = new Point(337, 90);
+            btnredis.Name = "btnredis";
+            btnredis.Size = new Size(206, 81);
+            btnredis.TabIndex = 0;
+            btnredis.Text = "REDIS";
+            btnredis.UseVisualStyleBackColor = true;
+            btnredis.Click += btnredis_Click;
+            // 
+            // btnsql
+            // 
+            btnsql.Location = new Point(635, 90);
+            btnsql.Name = "btnsql";
+            btnsql.Size = new Size(206, 81);
+            btnsql.TabIndex = 0;
+            btnsql.Text = "MSSQL";
+            btnsql.UseVisualStyleBackColor = true;
+            btnsql.Click += btnsql_Click;
+            // 
+            // txtrabbit
+            // 
+            txtrabbit.Location = new Point(40, 177);
+            txtrabbit.Name = "txtrabbit";
+            txtrabbit.Size = new Size(206, 23);
+            txtrabbit.TabIndex = 1;
+            txtrabbit.Text = "Server Name / Host IP";
+            txtrabbit.Click += txtrabbit_Click;
+            // 
+            // txtredis
+            // 
+            txtredis.Location = new Point(337, 177);
+            txtredis.Name = "txtredis";
+            txtredis.Size = new Size(206, 23);
+            txtredis.TabIndex = 1;
+            txtredis.Text = "Server Name / Host IP";
+            txtredis.Click += txtredis_Click;
+            // 
+            // txtsqlswname
+            // 
+            txtsqlswname.Location = new Point(635, 177);
+            txtsqlswname.Name = "txtsqlswname";
+            txtsqlswname.Size = new Size(206, 23);
+            txtsqlswname.TabIndex = 1;
+            txtsqlswname.Text = "Server Name / Host IP";
+            txtsqlswname.Click += txtsqlswname_Click;
+            // 
+            // txtsqlswpass
+            // 
+            txtsqlswpass.Location = new Point(741, 206);
+            txtsqlswpass.Name = "txtsqlswpass";
+            txtsqlswpass.Size = new Size(100, 23);
+            txtsqlswpass.TabIndex = 2;
+            txtsqlswpass.Text = "Password";
+            txtsqlswpass.Click += txtsqlswpass_Click;
+            // 
+            // txtsqlswuname
+            // 
+            txtsqlswuname.Location = new Point(635, 206);
+            txtsqlswuname.Name = "txtsqlswuname";
+            txtsqlswuname.Size = new Size(100, 23);
+            txtsqlswuname.TabIndex = 2;
+            txtsqlswuname.Text = "Username";
+            txtsqlswuname.Click += txtsqlswuname_Click;
+            // 
+            // chksqlswpass
+            // 
+            chksqlswpass.AutoSize = true;
+            chksqlswpass.Location = new Point(746, 235);
+            chksqlswpass.Name = "chksqlswpass";
+            chksqlswpass.Size = new Size(95, 19);
+            chksqlswpass.TabIndex = 3;
+            chksqlswpass.Text = "Şifreyi Göster";
+            chksqlswpass.UseVisualStyleBackColor = true;
+            chksqlswpass.CheckedChanged += chksqlswpass_CheckedChanged_1;
+            // 
+            // btnELK
+            // 
+            btnELK.Location = new Point(337, 262);
+            btnELK.Name = "btnELK";
+            btnELK.Size = new Size(206, 79);
+            btnELK.TabIndex = 4;
+            btnELK.Text = "Elasticsearch Send Test Data";
+            btnELK.UseVisualStyleBackColor = true;
+            btnELK.Click += btnELK_Click;
+            // 
+            // txtELK
+            // 
+            txtELK.Location = new Point(40, 347);
+            txtELK.Name = "txtELK";
+            txtELK.Size = new Size(206, 23);
+            txtELK.TabIndex = 5;
+            txtELK.Text = "Elasticsearch URL";
+            txtELK.Click += txtELK_Click;
+            // 
+            // txtIndex
+            // 
+            txtIndex.Location = new Point(337, 347);
+            txtIndex.Name = "txtIndex";
+            txtIndex.Size = new Size(206, 23);
+            txtIndex.TabIndex = 6;
+            txtIndex.Text = "Create İndex Name";
+            txtIndex.Click += txtIndex_Click;
+            // 
+            // txtUname
+            // 
+            txtUname.Location = new Point(40, 376);
+            txtUname.Name = "txtUname";
+            txtUname.Size = new Size(100, 23);
+            txtUname.TabIndex = 7;
+            txtUname.Text = "Username";
+            txtUname.Click += txtUname_Click;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(146, 376);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(100, 23);
+            txtPassword.TabIndex = 8;
+            txtPassword.Text = "Password";
+            txtPassword.Click += txtPassword_Click;
+            // 
+            // txtLevel
+            // 
+            txtLevel.Location = new Point(337, 376);
+            txtLevel.Name = "txtLevel";
+            txtLevel.Size = new Size(100, 23);
+            txtLevel.TabIndex = 9;
+            txtLevel.Text = "Level";
+            txtLevel.Click += txtLevel_Click;
+            // 
+            // txtMessage
+            // 
+            txtMessage.Location = new Point(443, 376);
+            txtMessage.Multiline = true;
+            txtMessage.Name = "txtMessage";
+            txtMessage.Size = new Size(100, 23);
+            txtMessage.TabIndex = 10;
+            txtMessage.Text = "Message";
+            txtMessage.Click += txtMessage_Click;
+            // 
+            // btnelk_con
+            // 
+            btnelk_con.Location = new Point(40, 262);
+            btnelk_con.Name = "btnelk_con";
+            btnelk_con.Size = new Size(206, 79);
+            btnelk_con.TabIndex = 11;
+            btnelk_con.Text = "Elasticsearch Connection";
+            btnelk_con.UseVisualStyleBackColor = true;
+            btnelk_con.Click += btnelk_con_Click;
+            // 
+            // chkelk
+            // 
+            chkelk.AutoSize = true;
+            chkelk.Location = new Point(151, 405);
+            chkelk.Name = "chkelk";
+            chkelk.Size = new Size(95, 19);
+            chkelk.TabIndex = 12;
+            chkelk.Text = "Şifreyi Göster";
+            chkelk.UseVisualStyleBackColor = true;
+            chkelk.CheckedChanged += chkelk_CheckedChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(905, 476);
+            Controls.Add(chkelk);
+            Controls.Add(btnelk_con);
+            Controls.Add(txtMessage);
+            Controls.Add(txtLevel);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUname);
+            Controls.Add(txtIndex);
+            Controls.Add(txtELK);
+            Controls.Add(btnELK);
+            Controls.Add(chksqlswpass);
+            Controls.Add(txtsqlswuname);
+            Controls.Add(txtsqlswpass);
+            Controls.Add(txtsqlswname);
+            Controls.Add(txtredis);
+            Controls.Add(txtrabbit);
+            Controls.Add(btnsql);
+            Controls.Add(btnredis);
+            Controls.Add(btnrabbit);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnrabbit;
+        private Button btnredis;
+        private Button btnsql;
+        private TextBox txtrabbit;
+        private TextBox txtredis;
+        private TextBox txtsqlswname;
+        private TextBox txtsqlswpass;
+        private TextBox txtsqlswuname;
+        private CheckBox chksqlswpass;
+        private Button btnELK;
+        private TextBox txtELK;
+        private TextBox txtIndex;
+        private TextBox txtUname;
+        private TextBox txtPassword;
+        private TextBox txtLevel;
+        private TextBox txtMessage;
+        private Button btnelk_con;
+        private CheckBox chkelk;
+    }
+}
---
+++ Test/Form1.cs
@@ -0,0 +1,337 @@
+﻿using System.Net;
+using System.Text;
+using RabbitMQ.Client;
+using StackExchange.Redis;
+using System.Data.SqlClient;
+using Nest;
+using Elasticsearch.Net;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        private const int DefaultRedisPort = 6379; // Default Redis port
+        private ConnectionMultiplexer redisConnection;
+        private static readonly Int32 db = 0; // default redis database index
+        private IElasticClient _elasticClient;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtrabbit_Click(object sender, EventArgs e)
+        {
+            txtrabbit.Text = string.Empty;
+        }
+
+        private void btnrabbit_Click(object sender, EventArgs e)
+        {
+            string rabbitMQHostName = txtrabbit.Text;
+            if (!string.IsNullOrWhiteSpace(rabbitMQHostName))
+            {
+                if (IsValidIPAddress(rabbitMQHostName))
+                {
+                    try
+                    {
+                        var factory = new ConnectionFactory() { HostName = rabbitMQHostName };
+
+                        using (var conn = factory.CreateConnection())
+                        using (var channel = conn.CreateModel())
+                        {
+                            channel.QueueDeclare(queue: "Test",
+                                                 durable: false,
+                                                 exclusive: false,
+                                                 autoDelete: false,
+                                                 arguments: null);
+
+                            string message = "Selam, RabbitMQ!";
+
+                            byte[] body = Encoding.UTF8.GetBytes(message);
+
+                            channel.BasicPublish(exchange: "", routingKey: "selam", basicProperties: null, body: body);
+
+                            MessageBox.Show("Mesaj başarıyla gönderildi.");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Bağlantı Hatası: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Geçersiz IP adres formatı. Geçerli formatta IP adresi girilmelidir.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("IP adres girilmelidir, boş bırakılamaz. Geçerli IP adresi girilmelidir.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public static bool IsValidIPAddress(string ipAddressString)
+        {
+            IPAddress? ipAddress = null;
+            return IPAddress.TryParse(ipAddressString, out ipAddress) && !IPAddress.IsLoopback(ipAddress);
+        }
+
+        //*******************************************************-------------------REDİS-------------------------*********************************************
+        private void txtredis_Click(object sender, EventArgs e)
+        {
+            txtredis.Text = string.Empty;
+        }
+        private void btnredis_Click(object sender, EventArgs e)
+        {
+            string redisServerIP = txtredis.Text.Trim();
+
+            // Check if the text box is empty
+            if (string.IsNullOrWhiteSpace(redisServerIP))
+            {
+                MessageBox.Show("IP adres girilmelidir, boş bırakılamaz. Geçerli IP adresi girilmelidir.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!IsValidIpAddress(redisServerIP))
+            {
+                MessageBox.Show("Geçersiz IP adres formatı. Geçerli formatta IP adresi girilmelidir.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Create a connection to the Redis server with the default port
+            var redisOptions = new ConfigurationOptions
+            {
+                EndPoints = { { redisServerIP, DefaultRedisPort } },
+                AbortOnConnectFail = false,
+            };
+
+            try
+            {
+                // Connect to Redis
+                redisConnection = ConnectionMultiplexer.Connect(redisOptions);
+
+                // Test Redis connection
+                var db = redisConnection.GetDatabase();
+                db.StringSet("sampleKey", "Hello, Redis!");
+
+                // Retrieve the data from Redis
+                var value = db.StringGet("sampleKey");
+
+                MessageBox.Show("Redis bağlantısı başarıyla gerçekleştirildi.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Bağlantı Hatası: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Function to check if a string is a valid IP address
+        private bool IsValidIpAddress(string ipAddress)
+        {
+            IPAddress? address;
+            return IPAddress.TryParse(ipAddress, out address);
+        }
+
+        //*******************************************************-------------------MSSQL-------------------------*********************************************
+
+        private void txtsqlswpass_Click(object sender, EventArgs e)
+        {
+            txtsqlswpass.Text = string.Empty;
+            this.txtsqlswpass.PasswordChar = '●';
+        }
+        private void chksqlswpass_CheckedChanged_1(object sender, EventArgs e)
+        {
+            txtsqlswpass.PasswordChar = chksqlswpass.Checked ? '\0' : '●';
+        }
+        private void txtsqlswuname_Click(object sender, EventArgs e)
+        {
+            txtsqlswuname.Text = string.Empty;
+        }
+        private void txtsqlswname_Click(object sender, EventArgs e)
+        {
+            txtsqlswname.Text = string.Empty;
+        }
+
+
+        private void btnsql_Click(object sender, EventArgs e)
+        {
+            string serverName = txtsqlswname.Text.Trim();
+            string username = txtsqlswuname.Text.Trim(); // Replace with your SQL Server username
+            string password = txtsqlswpass.Text; // Replace with your SQL Server password
+
+            // Database and table names
+            string databaseName = "NewDatabase";
+            string tableName = "NewTable";
+
+            if (string.IsNullOrWhiteSpace(serverName))
+            {
+                MessageBox.Show("IP adres girilmelidir, boş bırakılamaz. Geçerli IP adresi girilmelidir.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Check if the server name is a valid IP address or hostname
+            if (!IsValidIP(serverName) && !IsValidHostname(serverName))
+            {
+                MessageBox.Show("IP adres girilmelidir, boş bırakılamaz. Geçerli IP adresi girilmelidir.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Connection string for the master database
+            string masterConnectionString = $"Data Source={serverName};Initial Catalog=master;User ID={username};Password={password}";
+
+            // SQL statements to create the database and table
+            string createDatabaseSql = $"CREATE DATABASE [{databaseName}]";
+            string createTableSql = $"USE [{databaseName}]; CREATE TABLE [{tableName}] (Id INT IDENTITY(1,1), ColumnName NVARCHAR(255))";
+
+            using (SqlConnection masterConnection = new SqlConnection(masterConnectionString))
+            {
+                try
+                {
+                    masterConnection.Open();
+
+                    // Create the new database
+                    using (SqlCommand createDbCommand = new SqlCommand(createDatabaseSql, masterConnection))
+                    {
+                        createDbCommand.ExecuteNonQuery();
+                    }
+
+                    // Switch to the new database
+                    masterConnection.ChangeDatabase(databaseName);
+
+                    // Create a table in the new database
+                    using (SqlCommand createTableCommand = new SqlCommand(createTableSql, masterConnection))
+                    {
+                        createTableCommand.ExecuteNonQuery();
+                    }
+
+                    MessageBox.Show($"Sql server bağlantısı gerçekleştirildi. Test için database ve table oluşturuldu.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Bağlantı Hatası: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private bool IsValidIP(string ipAddress)
+        {
+            IPAddress? addr;
+            return IPAddress.TryParse(ipAddress, out addr);
+        }
+
+        private bool IsValidHostname(string hostname)
+        {
+            try
+            {
+                IPHostEntry host = Dns.GetHostEntry(hostname);
+                return host.AddressList.Length > 0;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        //*******************************************************-------------------ELASTİCSEARCH-------------------------*********************************************
+        private void txtPassword_Click(object sender, EventArgs e)
+        {
+            txtPassword.Text = string.Empty;
+            this.txtPassword.PasswordChar = '●';
+        }
+
+        private void chkelk_CheckedChanged(object sender, EventArgs e)
+        {
+            txtPassword.PasswordChar = chkelk.Checked ? '\0' : '●';
+        }
+
+        private void txtELK_Click(object sender, EventArgs e)
+        {
+            txtELK.Text = string.Empty;
+        }
+
+        private void txtUname_Click(object sender, EventArgs e)
+        {
+            txtUname.Text = string.Empty;
+        }
+
+        private void txtIndex_Click(object sender, EventArgs e)
+        {
+            txtIndex.Text = string.Empty;
+        }
+
+        private void txtLevel_Click(object sender, EventArgs e)
+        {
+            txtLevel.Text = string.Empty;
+        }
+
+        private void txtMessage_Click(object sender, EventArgs e)
+        {
+            txtMessage.Text = string.Empty;
+        }
+
+        private void InitializeElasticsearchClient()
+        {
+            try
+            {
+                var elasticsearchUrl = txtELK.Text;
+                var indexName = txtIndex.Text;
+                var username = txtUname.Text;
+                var password = txtPassword.Text;
+
+                // Elasticsearch bağlantı ayarlarını oluştur
+                var settings = new ConnectionSettings(new Uri(elasticsearchUrl))
+                    .BasicAuthentication(username, password);
+
+                _elasticClient = new ElasticClient(settings);
+
+                MessageBox.Show("Elasticsearch bağlantısı başarıyla kuruldu.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Elasticsearch bağlantısı kurulurken bir hata oluştu: {ex.Message}");
+            }
+        }
+        private void btnelk_con_Click(object sender, EventArgs e)
+        {
+            InitializeElasticsearchClient();
+        }
+        private void btnELK_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (_elasticClient == null)
+                {
+                    MessageBox.Show("Elasticsearch bağlantısı kurulu değil. Önce bağlantıyı kurun.");
+                    return;
+                }
+
+                // Test verisi oluştur
+                var testData = new
+                {
+                    Timestamp = DateTime.UtcNow,
+                    Message = txtMessage.Text,  // txtMessage adında bir TextBox kullanıldığından emin olun
+                    Level = txtLevel.Text  // txtLevel adında bir TextBox kullanıldığından emin olun
+                };
+
+                // Elasticsearch'e test verisini gönder
+                var indexResponse = _elasticClient.Index(testData, idx => idx
+                    .Index(txtIndex.Text)
+                );
+
+                if (indexResponse.IsValid)
+                {
+                    MessageBox.Show("Test verisi Elasticsearch'e başarıyla gönderildi.");
+                    // Formu yenile
+                    this.Invalidate();
+                }
+                else
+                {
+                    MessageBox.Show($"Test verisi gönderirken bir hata oluştu: {indexResponse.DebugInformation}");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Test verisi gönderirken bir hata oluştu: {ex.Message}");
+            }
+        }
+    }
+}
